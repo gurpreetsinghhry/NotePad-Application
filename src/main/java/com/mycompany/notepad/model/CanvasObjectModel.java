b1670bc83b2f8fb4f652e7f10f87dca6db11ad29
rath23 <sayyadmuneerahmad786@gmail.com>
2024-06-11 09:26:53 +0530

    Adding NotBlank anotation on canvas object

--- src/main/java/com/mycompany/notepad/model/CanvasObjectModel.java
+++ src/main/java/com/mycompany/notepad/model/CanvasObjectModel.java
@@ -2,11 +2,13 @@ package com.mycompany.notepad.model;
 
 import org.springframework.data.annotation.Id;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class CanvasObjectModel {
     @Id
+    @NotBlank
     private String id;
     private String canvasobjectType;
     private double xCoordinate;
