6282242998af53976ebfc07a20373cceaee44c7c
arskhushbookushwaha <159227473+arskhushbookushwaha@users.noreply.github.com>
2024-06-14 11:25:14 +0530

    CORS error Fix

--- src/main/java/com/mycompany/notepad/config/CorsConfiguration.java
+++ src/main/java/com/mycompany/notepad/config/CorsConfiguration.java
@@ -1,17 +1,12 @@
 package com.mycompany.notepad.config;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+@Configuration
 public class CorsConfiguration {
-
-    public static void main(String[] args) {
-        SpringApplication.run(CorsConfiguration.class, args);
-    }
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
