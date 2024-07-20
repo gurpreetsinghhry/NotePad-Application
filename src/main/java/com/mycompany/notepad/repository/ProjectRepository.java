077f00e6e9ecd2cf54ec1aeca2596fbf76643efa
rath23 <sayyadmuneerahmad786@gmail.com>
2024-07-20 21:38:56 +0530

    Adding API to add list of objects to the projects entity.

--- src/main/java/com/mycompany/notepad/repository/ProjectRepository.java
+++ src/main/java/com/mycompany/notepad/repository/ProjectRepository.java
@@ -1,7 +1,6 @@
 package com.mycompany.notepad.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.stereotype.Repository;
 
 import com.mycompany.notepad.entity.ProjectEntity;
--- src/main/java/com/mycompany/notepad/service/ProjectService.java
+++ src/main/java/com/mycompany/notepad/service/ProjectService.java
@@ -8,27 +8,27 @@ import com.mycompany.notepad.model.ProjectModel;
 import org.springframework.http.ResponseEntity;
 
 public interface ProjectService {
-    public ProjectEntity createProject(ProjectModel projectModel);
+     ProjectEntity createProject(ProjectModel projectModel);
 
-    public ResponseEntity<?> addCanvasObject(String projectId, CanvasObjectModel canvasObjectModel);
+     ResponseEntity<?> addCanvasObject(String projectId, CanvasObjectModel canvasObjectModel);
 
-    public ResponseEntity<?> getCanvasObjectById(String projectId, String canvasObjectId);
+     ResponseEntity<?> getCanvasObjectById(String projectId, String canvasObjectId);
 
-    public ResponseEntity<?> getCanvasObjectsByProjectId(String projectId);
+     ResponseEntity<?> getCanvasObjectsByProjectId(String projectId);
 
-    public ResponseEntity<?> getProjectById(String projectId);
+     ResponseEntity<?> getProjectById(String projectId);
 
-    public List<ProjectEntity> getAllProjects();
+     List<ProjectEntity> getAllProjects();
 
-    public ResponseEntity<?> updateProject(String projectId, ProjectModel projectModel);
+     ResponseEntity<?> updateProject(String projectId, ProjectModel projectModel);
 
-    public ResponseEntity<?> deleteProject(String projectId);
+     ResponseEntity<?> deleteProject(String projectId);
 
-    public ResponseEntity<?> updateCanvasObject(String projectId, String canvasObjectId,
+     ResponseEntity<?> updateCanvasObject(String projectId, String canvasObjectId,
             CanvasObjectModel canvasObjectModel);
 
-    public ResponseEntity<?> deleteCanvasObject(String projectId, String canvasObjectId);
+     ResponseEntity<?> deleteCanvasObject(String projectId, String canvasObjectId);
 
-    public ResponseEntity<?> addMultiObjects(String projectId, List<CanvasObjectModel> listOfCanvas);
+     ResponseEntity<?> addMultiObjects(String projectId, List<CanvasObjectModel> listOfCanvas);
 
 }
